da0ee552c3cc71f97c2042a9c4348f33abec5b46
hebusch <hwbusch@uc.cl>
2019-12-17 07:03:43 -0300

    modificado extension del archivo 'sound' para reducir peso

--- cliente/paths.py
+++ cliente/paths.py
@@ -1,6 +1,6 @@
 import os
 
 icon_path = os.path.join('assets','chat-17.ico')
-sound_path = os.path.join('assets','sound.wav')
+sound_path = os.path.join('assets','message_recieved.mp3')
 fondo_path = os.path.join('assets','negro.jpg')
 label_gris_path = os.path.join('assets','gris.jpg')
